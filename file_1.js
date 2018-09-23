d654bd9e5c6c31e0ed9041a4c485b1f4d91d079b
wasim963 <wasimcasper17@gmail.com>
2018-09-23 13:58:21 +0530

    File1 and file2 changed

--- file_1.js
+++ file_1.js
@@ -1,4 +1,5 @@
 function fun(){
 	console.log("This is my first file");
 	console.log("This is ist change in file_1");
+	console.log("change in both file_1 and File_2");
 }
--- file_2.js
+++ file_2.js
@@ -1,4 +1,5 @@
 function fun(){
 	console.log("This is my second file");
 	console.log("This is ist change in file_2");
+	console.log("change in both file_1 and File_2");
 }
